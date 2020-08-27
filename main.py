f132256d3ba4a8181041cee9e71bc912342a1333
Wojciech Siekiera <wsiekiera@gmail.com>
2020-08-27 14:37:17 +0200

    HFWC#2 PRD Deploy

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class DiscordMessageSender(commands.Bot):
 
     async def send_message(self):
         await self.wait_until_ready()
-        channel = self.get_channel(self.uat_channel_id)
+        channel = self.get_channel(self.hfwc_channel_id)
         while True:
             messages_list = self.database.get_messages_list()
             for item in messages_list:
